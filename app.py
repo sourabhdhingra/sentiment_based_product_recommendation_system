96d746014f9f6c0db40b62bef40c76a9d4e1a52e
Sourabh Dhingra <sourabhdhingra91@gmail.com>
2021-07-11 15:31:42 +0530

    debugging joblib for clf

--- app.py
+++ app.py
@@ -16,14 +16,15 @@ def predict():
     # Get values from browser
     username = request.args['username']
     user_id = model.get_user_id(username)
-    product_ids = model.get_top_20_recommendations(user_id)
-    top_5 = model.filter_top_5_recommendations(product_ids)
-    display_result = top_5[['product_id', 'name_brand']]
-    display_result[['product_name', 'brand']] = display_result['name_brand'].str.split('|',expand=True)
-    display_result.drop(['name_brand','product_id'], inplace=True, axis=1)
-    display_result_dict = display_result.to_dict('records')
-    col_names = display_result.columns.values
-    return render_template('index.html', records=display_result_dict, colnames=col_names)
+    return str(user_id)
+    # product_ids = model.get_top_20_recommendations(user_id)
+    # top_5 = model.filter_top_5_recommendations(product_ids)
+    # display_result = top_5[['product_id', 'name_brand']]
+    # display_result[['product_name', 'brand']] = display_result['name_brand'].str.split('|',expand=True)
+    # display_result.drop(['name_brand','product_id'], inplace=True, axis=1)
+    # display_result_dict = display_result.to_dict('records')
+    # col_names = display_result.columns.values
+    # return render_template('index.html', records=display_result_dict, colnames=col_names)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- model.py
+++ model.py
@@ -35,7 +35,7 @@ class LRModel:
 class Model:
     
     def __init__(self):
-        self.lr_clf_pipeline = joblib.load('logistic_user_sentiment_classifer_pipeline.sav')
+        # self.lr_clf_pipeline = joblib.load('logistic_user_sentiment_classifer_pipeline.sav')
         self.user_based_recommender = joblib.load('user_based_recommender.sav')
         self.username_bidict = joblib.load('username_bidict.sav')
         self.master_data = joblib.load('master_data.sav')
@@ -53,38 +53,38 @@ class Model:
         top_20_recommendations = self.user_based_recommender.recommend_top_20_products(user_id)
         return top_20_recommendations
 
-    def filter_top_5_recommendations(self, product_ids):
-        '''
-        Will consume top 20 recommended products
-        perform sentiment analysis
-        and return top 5 products based upon percentage/mean sentiment score
-        '''
+    # def filter_top_5_recommendations(self, product_ids):
+    #     '''
+    #     Will consume top 20 recommended products
+    #     perform sentiment analysis
+    #     and return top 5 products based upon percentage/mean sentiment score
+    #     '''
 
-        # filtering dataframe for our product ids
-        top_20 = self.master_data.query('product_id in {}'.format(list(product_ids.index)))
+    #     # filtering dataframe for our product ids
+    #     top_20 = self.master_data.query('product_id in {}'.format(list(product_ids.index)))
 
-        # making sentiment predictions for top_20
-        top_20_pred = self.lr_clf_pipeline.predict(top_20['reviews_text'])
+    #     # making sentiment predictions for top_20
+    #     top_20_pred = self.lr_clf_pipeline.predict(top_20['reviews_text'])
 
-        # assigning predictions to a column 'pred_user_sentiment'
-        top_20['pred_user_sentiment'] = pd.Series(top_20_pred, dtype=int)
+    #     # assigning predictions to a column 'pred_user_sentiment'
+    #     top_20['pred_user_sentiment'] = pd.Series(top_20_pred, dtype=int)
 
-        # imputing NaN values as per review ratings
-        top_20['pred_user_sentiment'] = top_20.apply(lambda x: self.imputer(x), axis=1 )
+    #     # imputing NaN values as per review ratings
+    #     top_20['pred_user_sentiment'] = top_20.apply(lambda x: self.imputer(x), axis=1 )
         
-        # preparing final_result
-        final_result = top_20[['product_id', 'pred_user_sentiment']].groupby('product_id').mean()
-        final_result = final_result.sort_values(by='pred_user_sentiment', ascending=False)
-        final_result.reset_index(inplace=True)
+    #     # preparing final_result
+    #     final_result = top_20[['product_id', 'pred_user_sentiment']].groupby('product_id').mean()
+    #     final_result = final_result.sort_values(by='pred_user_sentiment', ascending=False)
+    #     final_result.reset_index(inplace=True)
         
-        # mapping product names with product ids on final_result dataset
-        get_name_brand = lambda x: pd.unique(top_20[top_20['product_id']==x]['name_brand'])[0]
-        final_result['name_brand'] = final_result.apply(lambda x: get_name_brand(x['product_id']), axis=1)
+    #     # mapping product names with product ids on final_result dataset
+    #     get_name_brand = lambda x: pd.unique(top_20[top_20['product_id']==x]['name_brand'])[0]
+    #     final_result['name_brand'] = final_result.apply(lambda x: get_name_brand(x['product_id']), axis=1)
         
-        # picking up top 5
-        top_5 = final_result.head(5)
+    #     # picking up top 5
+    #     top_5 = final_result.head(5)
         
-        return top_5
+    #     return top_5
 
 
     def imputer(self, row):
